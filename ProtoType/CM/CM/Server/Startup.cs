94c505bb02e438a6b946bb08031b5bc059765ddd
Omar Wasfi <alprincewasfi@gmail.com>
2021-10-18 18:48:11 +0200

    Adding the AdminClient
    Add a section in the startup in the serverSide that will make the server recognize all the clients

--- ProtoType/CM/CM/Server/Startup.cs
+++ ProtoType/CM/CM/Server/Startup.cs
@@ -17,6 +17,8 @@ using CM.Library.DBContexts;
 using CM.Library.DataModels;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components;
 
 namespace CM.Server
 {
@@ -94,7 +96,23 @@ namespace CM.Server
             services.AddControllersWithViews();
             services.AddRazorPages();
 
-           
+            // Server Side Blazor doesn't register HttpClient by default
+            // Thanks to Robin Sue - Suchiman https://github.com/Suchiman/BlazorDualMode
+            if (!services.Any(x => x.ServiceType == typeof(HttpClient)))
+            {
+                // Setup HttpClient for server side in a client side compatible fashion
+                services.AddScoped<HttpClient>(s =>
+                {
+                    // Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.
+                    var uriHelper = s.GetRequiredService<NavigationManager>();
+                    return new HttpClient
+                    {
+                        BaseAddress = new Uri(uriHelper.BaseUri)
+                    };
+                });
+            }
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
